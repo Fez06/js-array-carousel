3d85184117c17fcd8b7371ac1c2abd81a8fb6b2b
Fez06 <mattiafezzardi1993@gmail.com>
2023-02-28 00:22:36 +0100

    non riesco a caoire il motivo per cui non vede img

--- main.js
+++ main.js
@@ -13,31 +13,56 @@
 <div class="boximg"><img src="img/05.webp" alt="img"></div> */}
 
 
-const imgs = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
+const imgs = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
 
-let itemsContent = '';
+let content = '';
+let active = 0;
+
 
 const slider = document.querySelector('.container');
+
+
 
 for (let i = 0; i < imgs.length; i++) {
 
     console.log(imgs[i]);
+    content += `<div class="boximg"><img src="img/${imgs[i]}" alt="img"></div>`
+}
 
-    itemsContent += '<div class="boximg"><img src="img/01.webp" alt="img"></div>'
 
+slider.innerHTML += content;
+document.querySelector('.boximg').classList.add('show');
 
-};
-
-slider.innerHTML += itemsContent;
-document.querySelector(".boximg").classList.add('show');
 
 const pageup = document.querySelector('.fa-angle-up');
 const pagedown = document.querySelector('.fa-angle-down');
+const allBoxImg = document.querySelectorAll('.boximg');
+console.log(allBoxImg);
+
+
+
+
 
 pageup.addEventListener('click', function(){
-    alert('you go up');
+
+    if(active > 0) {
+        allBoxImg[active].classList.remove('show');
+        active--;
+        allBoxImg[active].classList.add('show');
+        console.log(active);
+    }
+    
 });
 
+
+
 pagedown.addEventListener('click', function(){
-    alert('you go down');
+    
+    if(active < allBoxImg.length - 1) {
+        allBoxImg[active].classList.remove('show');
+        active++;
+        allBoxImg[active].classList.add('show');
+        console.log(active);
+    }
+
 });
